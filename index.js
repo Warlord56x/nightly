0bb9a2249df794c7cf7b8294416bc50db0c50589
Aerobird98 <aerobird98x@gmail.com>
2018-02-23 22:51:50 +0100

    getting started

---
+++ index.js
@@ -0,0 +1,10 @@
+var cm = CodeMirror(document.body,{
+  value: '',
+  mode:  'null',
+  theme: 'nightly',
+  addModeClass: true,
+  lineNumbers: true,
+  matchBrackets: true,
+  autoCloseBrackets: true,
+  continueComments: true
+})
